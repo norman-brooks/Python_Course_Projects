c169a8ee8cf420e6a946759f87165c295fb8b8d7
norman-brooks <nbprogramming@outlook.com>
2025-04-22 15:43:00 -0700

    If Statement Challenge step 111

---
+++ if_state.py
@@ -0,0 +1,9 @@
+num1 = 17
+
+if num1 >= 21:
+    print('You are old enough to drink')
+    
+elif num1 >= 18:
+    print('You are old enough to vote')
+else:
+    print('You are not old enough to drink or vote')
