63d2058c1737255d53e8e7ec252ff9165400ab6f
norman-brooks <nbprogramming@outlook.com>
2025-04-22 15:01:34 -0700

    Tuples Challenge on Step 74

---
+++ Tuples_Challenge.py
@@ -0,0 +1,16 @@
+mytuple = ("Eagles", "Phillies", "Sixers", "Flyers")
+print(mytuple)
+# ('Eagles', 'Phillies', 'Sixers', 'Flyers')
+
+
+# Loop through tuple items by using a for loop
+
+for x in mytuple:
+  print(x)
+
+
+# Tuple count() Method
+thistuple = (1, 5, 9, 3,  8, 9, 1, 3, 7, 3, 3)
+x = thistuple.count(3)
+print(x)
+3
