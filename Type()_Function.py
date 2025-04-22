fe55e36cb5b76402c7cd9cfd510eb21dcc4677cb
norman-brooks <nbprogramming@outlook.com>
2025-04-22 15:25:29 -0700

    Type() Function Challenge step 85

---
+++ Type()_Function.py
@@ -0,0 +1,19 @@
+x = 365
+print(type(x))
+# int
+
+x = 20.5
+print(type(x))
+# float
+
+x = "Hello World"
+print(type(x))
+# str
+
+x = ["Moe", "Larry", "Curly"]
+print(type(x))
+# list
+
+x = ("Bart", "Lisa", "Maggie")
+print(type(x))
+# tuple
