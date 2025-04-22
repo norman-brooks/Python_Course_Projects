77a522666a6dd9d7e03c07a666565eb2902f2fd8
norman-brooks <nbprogramming@outlook.com>
2025-04-22 14:42:42 -0700

    Operator's Challenge Step 69

---
+++ Operators_Challenge.py
@@ -0,0 +1,51 @@
+# Arithmetic Operator
+10 / 2
+5.0
+10 % 3
+1
+10 % 4
+2
+# Assignment Operator
+a = 12
+b = 6
+c = a + b
+print(c)
+18
+# Comparison Operator
+a = 12
+b = 6
+a > b
+True
+b > a
+False
+# Logical Operator
+a = 12
+b = 6
+a < 20 and a < 15
+True
+print(a > 12 or b == 6)
+True
+# Python Identity Operators
+x = ["mango", "banana"]
+y = ["mango", "banana"]
+z = x
+print(x is z)
+True
+print(x is y)
+False
+print(x == y)
+True
+# Python Membership Operators
+x = ["mango", "banana"]
+y = ["mango"]
+print ("mango" in x)
+True
+print ("banana" not in y)
+True
+# Bitwise Operators
+a = 10
+b = 4
+print("a & b =", a & b)
+a & b == 0
+print("a | b =", a | b)
+a | b == 14
