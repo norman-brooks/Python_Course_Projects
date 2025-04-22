25d2586cfe563ce09b50f17e4a988570b0ec8303
norman-brooks <nbprogramming@outlook.com>
2025-04-22 15:54:11 -0700

    While Loop Challenge Step 115

---
+++ While.py
@@ -0,0 +1,28 @@
+# Break Statement
+
+a = 1
+while a < 6:
+  print(a)
+  if (a == 5):
+    break
+  a +=1
+
+# Continue Statement
+
+b = 0
+while b < 9:
+  if b % 1 == 0:
+    continue
+  print(b)
+
+
+
+# Else Statement
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
